594c4d44a31eb965edfc05a7eb016a1346a266f6
cpp <cpp@example.com>
2019-09-03 08:59:52 +0800

    集成logger

--- src/main/java/com/cpp/learn/LearnSomeApplication.java
+++ src/main/java/com/cpp/learn/LearnSomeApplication.java
@@ -1,18 +1,21 @@
 package com.cpp.learn;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class
 })
-@MapperScan(" com.cpp.learn.dao")
 public class LearnSomeApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LearnSomeApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LearnSomeApplication.class, args);
+        LOGGER.error("application started");
     }
 
 }
