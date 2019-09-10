035d4f135d55450bb6b4037adfc78320798f8acc
cpp <cpp@example.com>
2019-09-10 08:10:35 +0800

    集成logger

--- src/main/java/com/cpp/learn/LearnSomeApplication.java
+++ src/main/java/com/cpp/learn/LearnSomeApplication.java
@@ -1,5 +1,6 @@
 package com.cpp.learn;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,9 +14,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class LearnSomeApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(LearnSomeApplication.class);
 
+    //获取指定名称的logger
+    private static final Logger LOGGER_HEALTH = LoggerFactory.getLogger("health_logger");
+
     public static void main(String[] args) {
         SpringApplication.run(LearnSomeApplication.class, args);
         LOGGER.error("application started");
+        LOGGER_HEALTH.info("app started");
     }
 
 }
